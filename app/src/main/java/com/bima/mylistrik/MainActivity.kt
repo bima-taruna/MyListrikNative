f49f3c61d5da5dbb5bf52adb06908a0d153f249d
Mhd Bima Taruna Cipta <bimataruna21@gmail.com>
2023-06-16 22:32:32 +0700

    fourth commit

--- app/src/main/java/com/bima/mylistrik/MainActivity.kt
+++ app/src/main/java/com/bima/mylistrik/MainActivity.kt
@@ -24,7 +24,6 @@ class MainActivity : ComponentActivity() {
                 ) {
                     Greeting("Android")
                 }
-                bima syang
             }
         }
     }
