2a5f1d4a320c81815262e07d985d26d133650354
Mhd Bima Taruna Cipta <bimataruna21@gmail.com>
2023-06-23 16:47:31 +0700

    eighth commit

--- app/src/main/java/com/bima/mylistrik/MainActivity.kt
+++ app/src/main/java/com/bima/mylistrik/MainActivity.kt
@@ -3,6 +3,7 @@ package com.bima.mylistrik
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -31,10 +32,13 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column() {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+        Text(text = "android dev")
+    }
 }
 
 @Preview(showBackground = true)
